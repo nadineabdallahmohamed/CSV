a593e1cff6b06b506ae266d880407ff93ffac4b8
Nadine Abdallah <64757359+nadineabdallahmohamed@users.noreply.github.com>
2021-05-28 00:16:06 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,24 @@
+package stream;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Solution {
+	 public static Map<String, City> hightest_pop_City(Map<String, List<City>> country_cities) {
+		 Map<String, City> country_pop = new HashMap<String, City>();
+		 country_cities.keySet().stream().forEach(m->
+			country_pop.put(m,
+			country_cities.get(m).stream().max((c,t)->c.getPopulation()> t.getPopulation()?1:-1).get())
+		 );
+		 return country_pop;
+	}
+	 public static City hightest_pop_capital(List<Country> countries ,List<City> cities) {
+		 List<Integer> countries_capitals = countries.stream().map(m->m.getCapital()).collect(Collectors.toList());
+		 return cities.stream().filter(t->countries_capitals.contains(t.getId())).max((c1,c2)->c1.getPopulation() >c2.getPopulation()?1:-1).get();
+	}
+ 
+}
